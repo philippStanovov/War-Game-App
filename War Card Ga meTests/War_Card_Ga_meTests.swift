ac8fb0c9c77fd5b8034f0b8e93ee77261c97d7c0
Philipp S. <philsus88@gmail.com>
2025-04-25 12:12:54 -0400

    Add files via upload

---
+++ War Card Ga meTests/War_Card_Ga_meTests.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Ga_meTests.swift
+//  War Card Ga meTests
+//
+//  Created by Philipp Stanovov on 4/16/25.
+//
+
+import Testing
+@testable import War_Card_Ga_me
+
+struct War_Card_Ga_meTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ War Card Game/War_Card_Ga_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Ga_meApp.swift
+//  War Card Ga me
+//
+//  Created by Philipp Stanovov on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Ga_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
